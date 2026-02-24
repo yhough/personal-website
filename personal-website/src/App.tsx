a7492e0cf60a42c892a60a3d1f63114b6454fc21
Yaelin <hough.yaelin@gmail.com>
2026-02-23 22:45:09 -0500

    updated title, removed unused assets, added framer-motion dependency, and implemented basic ui from wireframe

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import FolderStack from './components/FolderStack'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  const folders = [
+    { id: "about", color: "#ff8daa" },
+    { id: "experience", color: "#fdaeae" },
+    { id: "projects", color: "#f6e2b3" },
+    { id: "skills", color: "#b49982" },
+    { id: "contact", color: "#7e6651" }
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ padding: "80px", width: "100%", boxSizing: "border-box" }}>
+      <h1 style={{ textAlign: "center", marginBottom: "50px" }}>
+        hi! i'm yaelin hough :)
+      </h1>
+      <div style={{ width: "100%", marginTop: "20px" }}>
+        <FolderStack folders={folders} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
